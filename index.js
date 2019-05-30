d5e2aa6b5608de896d7612206c716f9378b2c907
Hariet <harietlewis@gmail.com>
2019-05-29 20:21:40 -0400

    Random Quote

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use(express.static('public'));
+
+const quotes = require('random-movie-quotes')
+
+app.get('/quote', async (req, res, next) => {
+  try {
+    const quote = await quotes.getQuote()
+    res.json(quote);
+  } catch (err) {
+    console.error(err)
+  }
+
+});
+
 
 app.get('/quote', (req, res) => {
   res.json({
     quote: quotes.getQuote()
   });
 });
+
+app.get('/random', function(request, response) {
+  let randomQuote = data[random(0, data.length - 1)]
+  if (randomQuote) response.json(randomQuote)
+})
+
 
 
 app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
--- public/main.js
+++ public/main.js
@@ -1,5 +1,14 @@
+var quotes = require('random-movie-quotes')
+
+
 fetch('/quote')
   .then(r => r.json())
   .then(data => {
     document.getElementById('quote').innerText = data.quote;
   });
+
+console.log(quotes.getQuote())
+console.log(`${data.quote}`)
+
+
+
